a098fef23d76314515b2ec7d3fbd86bf88e0846d
licholucero <licholucero7@gmail.com>
2020-07-03 13:46:33 -0300

    clase cliente y venta (todo arreglado)

--- Ejercicio2/ventas/models.py
+++ Ejercicio2/ventas/models.py
@@ -41,3 +41,21 @@ class Detalle(models.Model):
     cantidad = models.IntegerField()
     def __str__(self):
         return '{}'.format(self.producto)
+
+class Cliente(models.Model):
+    rut = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    telefono = models.IntegerField()
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    detalle = models.ManyToManyField(Detalle)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    monto = models.IntegerField()
+    descuento = models.BooleanField()
+    def __str__(self):
+        return '{}'.format(self.id)
