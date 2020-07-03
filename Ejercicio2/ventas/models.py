9046671a8757ca8f7ee8b64b15d320830c8c40d5
licholucero <licholucero7@gmail.com>
2020-07-03 13:19:42 -0300

    clase categoria

--- Ejercicio2/ventas/models.py
+++ Ejercicio2/ventas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=40)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
