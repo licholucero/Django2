aa9f760682614c09a6612792fb93f52476bbeb0f
licholucero <licholucero7@gmail.com>
2020-07-03 13:53:59 -0300

    Actividad 2 resuelta

--- Ejercicio2/ventas/admin.py
+++ Ejercicio2/ventas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from. models import *
 
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['rut', 'nombre','telefono',]
+    search_fields = ['nombre',]
+
+class ProductoInline(admin.TabularInline):
+    model = Producto
