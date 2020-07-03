2a7bf4c4c7001cb56522d94acc399d10219ac494
licholucero <licholucero7@gmail.com>
2020-07-03 13:24:32 -0300

    clase direccion y provedor

--- Ejercicio2/ventas/models.py
+++ Ejercicio2/ventas/models.py
@@ -9,3 +9,18 @@ class Categoria(models.Model):
     def __str__(self):
         return '{}'.format(self.nombre)
 
+class Direccion(models.Model):
+    calle = models.CharField(max_length=20)
+    numero = models.IntegerField()
+    ciudad = models.CharField(max_length=20)
+    def __str__(self):
+        return '{}'.format(self.calle)
+
+class Proveedor(models.Model):
+    rut = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    telefono = models.IntegerField()
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    web = models.CharField(max_length=40)
+    def __str__(self):
+        return '{}'.format(self.nombre)
